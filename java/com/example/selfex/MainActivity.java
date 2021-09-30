78ded0954087150ab95317faac2b967f77070cc1
wang-hsoo <64819868+wang-hsoo@users.noreply.github.com>
2021-10-01 04:19:08 +0900

    Add files via upload

---
+++ java/com/example/selfex/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.selfex;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.opengl.Visibility;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import java.util.Calendar;
+import android.app.DatePickerDialog;
+
+
+import com.example.selfex.R;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button button;
+    Button userButton1;
+    Button userButton2;
+    LinearLayout user1;
+    LinearLayout user2;
+
+//이미지 뷰, 메뉴누르면 전환
+    String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+        userButton1 = findViewById(R.id.userButton1);
+        userButton2 = findViewById(R.id.userButton2);
+        user1 = findViewById(R.id.User1);
+        user2 = findViewById(R.id.User2);
+
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
+                imageView.setVisibility(View.VISIBLE);
+                button.setVisibility(View.GONE);
+            }
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        userButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user1.setVisibility(View.VISIBLE);
+                user2.setVisibility(View.GONE);
+            }
+        });
+
+        userButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user2.setVisibility(View.VISIBLE);
+                user1.setVisibility(View.GONE);
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    Uri uri = data.getData();
+                    imageView.setImageURI(uri);
+                }
+        }
+    }
+}
+
+
+
